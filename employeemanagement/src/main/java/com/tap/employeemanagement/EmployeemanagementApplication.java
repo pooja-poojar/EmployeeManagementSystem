ce7451641d5ed8850f7281d4a7e7b432703f70ce
pooja poojar <145640745+pooja-poojar@users.noreply.github.com>
2024-02-28 11:29:01 +0530

    Add files via upload

---
+++ employeemanagement/src/main/java/com/tap/employeemanagement/EmployeemanagementApplication.java
@@ -0,0 +1,14 @@
+package com.tap.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeemanagementApplication.class, args);
+	}
+
+}
